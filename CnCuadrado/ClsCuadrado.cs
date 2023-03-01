9c5b48d9934ef428b85189b924b556afc1025d71
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-02-28 20:36:59 -0500

    Agregar archivos de proyecto.

---
+++ CnCuadrado/ClsCuadrado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnCuadrado
+{
+    internal class ClsCuadrado
+    {
+        //Variable
+        private double lado;
+
+        public double Lado { get => lado; set => lado = value; }
+
+        //Metodos
+        public void CargarDatos()
+        {
+            Console.WriteLine("ingresa el valor de un lado");
+            Lado = double.Parse(Console.ReadLine());
+        }
+
+        public double perimetro(double lado)
+        {
+            double perimetro1 = lado * 4;
+            return perimetro1;
+
+
+        }
+
+        public double superficie(double lado)
+        {
+            double superficie1 = lado * lado;
+            return superficie1;
+        }
+    }
+}
---
+++ CnCuadrado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnCuadrado
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Nombre:Viviana Muñoz
+        /// Fecha:26/02/2023
+        /// Descripcion:Programa que solicita el lado de un cuadrado e imprime el perimetro y superficie del mismo
+        /// </summary>
+        /// 
+        static void Main(string[] args)
+        {
+            //Instancia de clase
+            ClsCuadrado cuadrado = new ClsCuadrado();
+
+            //Llamada de metodos
+            cuadrado.CargarDatos();
+            Console.WriteLine("el valor del perimetro es: " + cuadrado.perimetro(cuadrado.Lado));
+            Console.WriteLine("El valor de la superficie es: " + cuadrado.superficie(cuadrado.Lado));
+
+
+            Console.ReadKey();
+        }
+    }
+}
